0ee1f8927aa4ef1036f0114d2724790857475fed
galvarezgbcc <120694254+galvarezgbcc@users.noreply.github.com>
2023-05-01 11:02:11 -0400

    Chapter 4

--- src/CountEvens.java
+++ src/CountEvens.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
-public class CountEvens {
+public class CountOdds {
 
-   /* Type your code here */
-   public static int countEvens(int n1, int n2, int n3, int n4){
+   /* It counts odds now */
+   public static int countOdds(int n1, int n2, int n3, int n4){
       int count = 0;
-      if(n1 % 2 == 0)
+      if(n1 % 2 == 1)
          count++;
-      if(n2 % 2 == 0)
+      if(n2 % 2 == 1)
          count++;
-      if(n3 % 2 == 0)
+      if(n3 % 2 == 1)
          count++;
-      if(n4 % 2 == 0)
+      if(n4 % 2 == 1)
          count++;
       return count;
    }
-
+/* I changed CountEvens to CountOdds */
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      CountEvens labObject = new CountEvens();
+      CountEvens labObject = new CountOdds();
 
       int num1;
       int num2;
@@ -30,8 +30,8 @@ public class CountEvens {
       num3 = scnr.nextInt();
       num4 = scnr.nextInt();
 
-      int result = labObject.countEvens(num1, num2, num3, num4);
-      System.out.println("Total evens: " + result);
+      int result = labObject.countOdds(num1, num2, num3, num4);
+      System.out.println("Total odds: " + result);
    }
 }
 
